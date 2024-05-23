9dab2b2586f564de855c8c5829f25a1e282812c5
Yusuf AĞAÇ <yusuftree1@hotmail.com>
2024-05-23 21:50:22 +0300

    feat(Editor): add subclass picker for selecting subclass from editor

---
+++ WheelSpinGame/Assets/Scripts/CustomEditor/SubclassPicker.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections;
+using System.Linq;
+using System.Reflection;
+using UnityEditor;
+using UnityEngine;
+
+#if UNITY_EDITOR
+public class SubclassPicker : PropertyAttribute { }
+
+[CustomPropertyDrawer(typeof(SubclassPicker))]
+public class SubclassPickerDrawer : PropertyDrawer
+{
+    public override float GetPropertyHeight(SerializedProperty property, GUIContent label)
+    {
+        return EditorGUI.GetPropertyHeight(property);
+    }
+
+    private static IEnumerable GetClasses(Type baseType)
+    {
+        if (baseType.IsArray) baseType = baseType.GetElementType();
+        return Assembly.GetAssembly(baseType).GetTypes().Where(t => t.IsClass && !t.IsAbstract && baseType.IsAssignableFrom(t));
+    }
+
+    public override void OnGUI(Rect position, SerializedProperty property, GUIContent label)
+    {
+        var t = fieldInfo.FieldType;
+        var typeName = property.managedReferenceValue?.GetType().Name ?? "Not set";
+
+        var dropdownRect = position;
+        dropdownRect.x += EditorGUIUtility.labelWidth + 2;
+        dropdownRect.width -= EditorGUIUtility.labelWidth + 2;
+        dropdownRect.height = EditorGUIUtility.singleLineHeight;
+        if (EditorGUI.DropdownButton(dropdownRect, new(typeName), FocusType.Keyboard))
+        {
+            var menu = new GenericMenu();
+
+            menu.AddItem(new GUIContent("None"), property.managedReferenceValue == null, () =>
+            {
+                property.managedReferenceValue = null;
+                property.serializedObject.ApplyModifiedProperties();
+            });
+
+            foreach (Type type in GetClasses(t))
+            {
+                menu.AddItem(new GUIContent(type.Name), typeName == type.Name, () =>
+                {
+                    property.managedReferenceValue = type.GetConstructor(Type.EmptyTypes).Invoke(null);
+                    property.serializedObject.ApplyModifiedProperties();
+                });
+            }
+            menu.ShowAsContext();
+        }
+        EditorGUI.PropertyField(position, property, label, true);
+    }
+}
+#endif
