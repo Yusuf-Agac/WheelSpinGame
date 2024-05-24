e8bc419edb07571dfee772ce0d95475022c8a01a
Yusuf AĞAÇ <yusuftree1@hotmail.com>
2024-05-24 05:14:15 +0300

    feat(Inventory): add inventory system

---
+++ WheelSpinGame/Assets/Scripts/Inventory/InventoryItem.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using UnityEngine;
+
+namespace Inventory
+{
+    [Serializable]
+    public class InventoryItem
+    {
+        public int id;
+        public string itemName;
+        public Sprite icon;
+        public int amount;
+        
+        public InventoryItem(int id, string itemName, Sprite icon, int amount)
+        {
+            this.id = id;
+            this.itemName = itemName;
+            this.icon = icon;
+            this.amount = amount;
+        }
+        
+        public InventoryItem(Item.Item item)
+        {
+            id = item.id;
+            itemName = item.itemName;
+            icon = item.icon;
+            amount = item.baseAmount;
+        }
+    }
+}
---
+++ WheelSpinGame/Assets/Scripts/Inventory/PlayersInventory.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using UnityEngine;
+
+namespace Inventory
+{
+    public class PlayersInventory : MonoBehaviour
+    {
+        public List<InventoryItem> items = new();
+
+        private void Awake()
+        {
+            LoadInventory();
+        }
+
+        public void AddItem(InventoryItem item)
+        {
+            if (items.Any(itemTemp => itemTemp.id == item.id))
+            {
+                items.Find(itemTemp => itemTemp.id == item.id).amount += item.amount;
+            }
+            else
+            {
+                items.Add(item);
+            }
+        }
+
+        public void AddItem(Item.Item item)
+        {
+            var inventoryItem = new InventoryItem(item);
+            AddItem(inventoryItem);
+        }
+
+        public void RemoveItem(InventoryItem item)
+        {
+            if (items.Any(itemTemp => itemTemp.id == item.id))
+            {
+                items.Find(itemTemp => itemTemp.id == item.id).amount -= item.amount;
+                if (items.Find(itemTemp => itemTemp.id == item.id).amount <= 0)
+                {
+                    items.Remove(item);
+                }
+            }
+        }
+        
+        public void SetItemAmount(InventoryItem item, int amount)
+        {
+            if (items.Any(itemTemp => itemTemp.id == item.id))
+            {
+                items.Find(itemTemp => itemTemp.id == item.id).amount = amount;
+            }
+            else
+            {
+                items.Add(item);
+            }
+        }
+        
+        public void SaveInventory()
+        {
+            var json = JsonUtility.ToJson(this);
+            File.WriteAllText(Application.persistentDataPath + "/inventory.json", json);
+        }
+
+        public void LoadInventory()
+        {
+            if (!File.Exists(Application.persistentDataPath + "/inventory.json")) return;
+            
+            var json = File.ReadAllText(Application.persistentDataPath + "/inventory.json");
+            JsonUtility.FromJsonOverwrite(json, this);
+        }
+    }
+}
